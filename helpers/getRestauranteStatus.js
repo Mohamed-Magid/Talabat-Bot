e2d4a836582cfda6123467d095d35d6cd3425258
Mohamed Magid <Mohamed-Magid@hotmail.com>
2024-01-13 20:06:31 +0200

    testing

--- helpers/getRestauranteStatus.js
+++ helpers/getRestauranteStatus.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-
+const fs = require('fs')
 const getRestaurantStatus = async (url) => {
     const response = await axios.get(url, {
         headers: {
@@ -8,6 +8,7 @@ const getRestaurantStatus = async (url) => {
         }
 
     });
+    fs.writeFileSync('test.html', response.data);
     const $ = cheerio.load(response.data);
     const busyStrings = ['مشغول', 'busy', 'closed', 'مغلق'];
     const status = $('span[data-testid="rest-status"]').text().trim();
